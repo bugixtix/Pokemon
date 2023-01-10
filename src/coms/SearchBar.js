0c311814288297fa2ae76f2daaca454ad73a2837
Housam_ <ixtix_bug@hotmail.com>
2023-01-10 23:32:23 +0100

    " init SearchBar com

---
+++ src/coms/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import {BiSearchAlt2} from 'react-icons/bi'
+export const SearchBar = () => {
+    let [inValue_$, setValue_$] = useState('')
+    function changeValue(e){
+        setValue_$(e.target.value)
+    }
+  return (
+    <div className='SB_'>
+        <input className='SB_in' type={'text'} placeholder='Pikachu ...' onChange={(e)=>changeValue(e)} value={inValue_$}/>
+        <button className='SB_btn'><BiSearchAlt2 className='SB_i'/></button>
+    </div>
+  )
+}
