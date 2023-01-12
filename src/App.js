9d8c03d67c268bee1bf2ced27e563d00e2178239
Housam_ <ixtix_bug@hotmail.com>
2023-01-13 00:37:08 +0100

    " #12012023

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Navbar/>
       <Main/>
       <SearchBar/>
-      {/* <Content/> */}
+      <Content/>
       {/* <Footer/> */}
     </div>
   );
--- src/coms/Content.js
+++ src/coms/Content.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Card } from './Card'
 
 export const Content = () => {
   return (
-    <div>Content</div>
+    <div className='C_'>
+        <h1 className='C_h1'> the latest released cards</h1>
+        <div className='C_C'>
+          <Card/>
+        </div>
+    </div>
   )
 }
