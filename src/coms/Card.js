d12ea311a801e96baa9b7bf39cf1d428e960278d
Housam_ <ixtix_bug@hotmail.com>
2023-01-13 23:49:23 +0100

    " #13012023 init a single Card com

--- src/coms/Card.js
+++ src/coms/Card.js
@@ -10,11 +10,22 @@ export const Card = () => {
     <div className='Card_'>
         <div className='Card_head'>
             <div className='head_con'>
-                <p className='head__type'>Basic Pokémon --type</p>
-                <h3 className='head__name'>Mewtwo --name</h3>
+                <p className='head__type'>Basic Pokémon 
+                {/* --type */}
+                </p>
+                <h3 className='head__name'>Mewtwo 
+                {/* --name */}
+                </h3>
             </div>
 
-            <div className='head_HP'> ?? HP ++icon</div>
+            <div className='head_HP'>
+                <div className='HP_con'>
+                <span className='HP_value'> ?? HP </span>
+                <span className='HP_icons'><BsFillEyeFill className='icon_HP'/></span>
+                </div>
+
+            {/* ++icon */}
+            </div>
         </div>
         
         <div className='Card_img-con'>
@@ -43,39 +54,84 @@ export const Card = () => {
         </div>
 
         <div className='Card_ability-2'>
-            <div className='ability-2_icons'>--icons</div>
+            <div className='ability-2_icons'>
+                <span className='icons_eye'><BsFillEyeFill className='eye_icon'/></span>
+                <span className='icons_eye-1'><BsFillEyeFill className='eye_icon'/></span>
+            </div>
             <div className='ability-2_con'>
-                <h3 className='ability-2__type'>Barrier --second_ability_type</h3>
-                <p className='ability-2__description'>Discard I Energy card attached to Mewtwo in order to use this attack. During your opponent's next turn, prevent all effects of attacks, including damage, done to Mewtwo.</p>
+                <div className='ability-2__description'>
+                {/* --second_ability_type */}
+                <span className='ability-2__type'> Barrier </span>
+                Discard I Energy card attached to Mewtwo in order to use this attack. During your opponent's next turn, prevent all effects of attacks, including damage, done to Mewtwo.
+                </div>
             </div>
         </div>
 
             <div className='Card_extra'>
-                <div className='extra_con1'>
-                    <p className='extra__weakness'>weakness --weakness</p>
-                    <img className='extra__weakness-img'/>
+                <div className='extra_con'>
+                <div className='extra_item-1'>
+                    <p className='extra__weakness'>weakness
+                    {/* --weakness */}
+                    </p>
+                    <span className='extra__weakness-icons'>
+                        <span className='weakness_icons'>
+                            <BsFillEyeFill className='icon_weakness'/>
+                        </span>
+                    </span>
                 </div>
-                <div className='extra_con2'>
-                    <p className='extra__resistance'>resistance --resistance</p>
-                    <img className='extra__resistance-img'/>
+                <div className='extra_item-2'>
+                    <p className='extra__resistance'>resistance 
+                    {/* --resistance */}
+                    </p>
+                    <span className='extra__resistance-icons'>
+                        <span className='resistance_icons'>
+                            <BsFillEyeFill className='icon_resistance'/>
+                        </span>
+                    </span>
                 </div>
-                <div className='extra_con3'>
-                    <p className='extra__recost'>retreat cost --recost</p>
-                    <img className='extra__recost-img'/>
+                <div className='extra_item-3'>
+                    <p className='extra__recost'>retreat cost 
+                    {/* --recost */}
+                    </p>
+                    <span className='extra__recost-icons'>
+                        <span className='recost_icons'>
+                            <HiStar className='icon_recost-1'/>
+                        </span>
+                        <span className='recost_icons'>
+                            <HiStar className='icon_recost-2'/>
+                        </span>
+                        <span className='recost_icons'>
+                            <HiStar className='icon_recost-3'/>
+                        </span>
+                    </span>
                 </div>
-            </div>
-
+                </div>
             <div className='Card_ori'>
-                <p className='ori_p'>Ascientist created this Pokémon after years of horrific genesplicing and DNA engineering experiments. --ori 
-                    <span className='ori_level'>LV.53 --level</span>
-                    <span className='ori_id'>#150 --id</span>
+                <p className='ori_p'>Ascientist created this Pokémon after years of horrific genesplicing and DNA engineering experiments. 
+                 {/*--ori  */}
+                    <span className='ori_level'> LV.53 
+                    {/* --level */}
+                    </span>
+                    <span className='ori_id'> #150 
+                    {/* --id */}
+                    </span>
                 </p>
+            </div>
             </div>
 
             <div className='Card_-info'>
-                <p className='info_illustrator'>iilus. Ken Sugimori --illustrator</p>
-                <p className='info_creator'>c1995. 96 98 Nintendo Creatures, GAMEFREAK c 1999 --creator</p>
-                <p className='info_end'>10/102 --unknown_number</p>
+                <p className='info_illustrator'>iilus. Ken Sugimori 
+                {/* --illustrator */}
+                </p>
+                <p className='info_creator'>c1995. 96 98 Nintendo Creatures, GAMEFREAK c 1999
+                {/*  --creator */}
+                </p>
+                <p className='info_end'>10/102
+                {/*  --unknown_number */} 
+                <span>
+                    <HiStar className='icon_end'/>
+                </span>
+                </p>
             </div>
 
     </div>
--- src/coms/Content.js
+++ src/coms/Content.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Card } from './Card'
+import '../App.css'
 
 export const Content = () => {
   return (
     <div className='C_'>
         <h1 className='C_h1'> the latest released cards</h1>
         <div className='C_C'>
+          <Card/>
           <Card/>
         </div>
     </div>
