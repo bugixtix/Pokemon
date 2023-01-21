a080ac6f664a7d99e44ca2d4d9a9a172ce510c29
Housam_ <ixtix_bug@hotmail.com>
2023-01-21 23:15:24 +0100

    " #21012023

--- src/coms/Content.js
+++ src/coms/Content.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from './Card'
 import '../App.css'
 
-export const Content = () => {
+export const Content = () => {  
+
+  var [pokemon_$, setPokemon_$]=useState([])
+
+  // useEffect(()=>{
+  //   //   for(let i = 1; i<3; i++){
+  //   //     fetch('https://pokeapi.co/api/v2/pokemon/'+i+'/').then(res=>res.json()).then((res)=>{setPokemon_$(prev=>[...prev,res])})
+  //   //   }
+  //   // },[])
+    console.log(pokemon_$)
+
   return (
     <div className='C_'>
         <h1 className='C_h1'> the latest released cards</h1>
