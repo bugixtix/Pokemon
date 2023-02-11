77abab2cc532d94f730d0d109409e2962b8f78dc
Housam_ <ixtix_bug@hotmail.com>
2023-02-11 09:33:32 +0100

    " #11022023

--- src/coms/Content.js
+++ src/coms/Content.js
@@ -9,20 +9,20 @@ export const Content = () => {
   var [ready_$, setReady_$] = useState(false)
   var [startIndex_$, setStart_$] = useState(0)
   var [endIndex_$, setEnd_$] = useState(1260)
+  var [array_$,setArray_$] = useState([])
   let allPoks = [];
   let url_20 = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=19'
   useEffect(()=>{
-      fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${startIndex_$}&limit=${startIndex_$+19}`).then(res=>res.json()).then((res_)=>{
-        setAll_$(res_.results);
-        for (let i = 1; i<all_$.length; i++){
-          fetch(all_$[i].url).then(res=>res.json()).then(res=>setPokemon_$(prev=>[...prev,res]))
-        }
+      fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${startIndex_$}&limit=${(startIndex_$+19)}`).then(res=>res.json()).then((res_)=>{
+      res_.results.forEach((i)=>{fetch(i.url).then(res=>res.json()).then(res__=>setArray_$(prev=>[...prev,res__]))})
+      // res_.results.forEach((i)=>{fetch(i.url).then(res=>res.json()).then(res=>setPokemon_$(prev=>[...prev,res]))})
       })
     },[startIndex_$])
-  useEffect(()=>{
-    // fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${0}&limit=${19}`).then(res=>res.json()).then((res_)=>{
-    //   setAll_$(res_.results);})
-    },[])
+  // useEffect(()=>{
+  //   console.log(startIndex_$)
+  //   all_$.forEach((i)=>{fetch(i.url).then(res=>res.json()).then(res_=>setArray_$(prev=>[...prev,res_]))})
+  //   setArray_$([])
+  //   },[all_$])
     // console.log(pokemon_$)
     // console.log(all_$)
     // 
@@ -40,11 +40,25 @@ export const Content = () => {
       startIndex_$<0 ? setStart_$(0) : startIndex_$>1279 ? setStart_$(1279) : console.log(`site nr. ${Math.round(startIndex_$/19)}`)
     },[startIndex_$])
 
+    function Fetchem ({pro}){
+      // console.log(all_$)
+      useEffect(()=>{
+        // all_$.forEach((i,index)=>{fetch(i.url).then(res=>res.json()).then(res=>setArray_$(res))})
+      },[allPoks])
+      return(
+        <span>
+          {/* {array_$.map(i=>(<h1> {i.name} </h1>))} */}
+        </span>
+      )
+    }
   return (
     <div className='C_'>
         <h1 className='C_h1'> the latest released cards</h1>
         <div className='C_C'>
-          {pokemon_$.map((x,index)=><Card key={index} name_={x.name} data_={x} index_={index}/>)}
+          {/* {pokemon_$.map((x,index)=><Card key={index} name_={x.name} data_={x} index_={index}/>)} */}
+          {/* {all_$.map((i,index)=>fetch(i.url).then(res=>res.json()).then(res=>(<Card key={index} name_={res.name} data_={res} index_={index}/>)))} */}
+          {/* {<Fetchem/>} */}
+          {array_$.map((i,index)=><Card index_={index} data_={i} name_={i.data_} key={index}/>)}
         </div>
         <div className='C_C1'>
           <button className='C_btn prev_'>
